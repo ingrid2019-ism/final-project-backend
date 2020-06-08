ba64bc89bb1317d85c168ee01fdfa77b75c06038
Ingrid Hammargren <ingrid.hammargren@gmail.com>
2020-06-08 12:20:08 +0200

    created mongoose model for book and user

--- server.js
+++ server.js
@@ -1,6 +1,61 @@
-import express from 'express'
+
+
+
 import bodyParser from 'body-parser'
 import cors from 'cors'
+import express from 'express'
+import mongoose from 'mongoose'
+import booksData from './books.json'
+
+const mongoUrl = process.env.MONGO_URL || 'mongodb://localhost/books1'
+mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.Promise = Promise
+
+//Create the data models here:
+const User = mongoose.model('User', {
+  name: {
+    type: String,
+    unique: true
+  },
+  email: {
+    type: String,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  accessToken: {
+    type: String,
+    default: () => crypto.randomBytes(128).toString('hex')
+  }
+})
+
+const Book = mongoose.model('Book', {
+  bookId: {
+    type: Number
+  },
+  title: {
+    type: String
+  },
+  authors: {
+    type: String
+  },
+  average_rating: {
+    type: Number
+  }
+})
+
+//Seed the database here:
+//if (process.env.RESET_DATABASE) {
+//console.log('Resetting my database!')
+
+const seedDatabase = async () => {
+  await Book.deleteMany()
+  booksData.forEach((book) => new Book(book).save())
+}
+seedDatabase()
+//}
 
 // Defines the port the app will run on. Defaults to 8080, but can be 
 // overridden when starting the server. For example:
@@ -22,3 +77,5 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`)
 })
+
+

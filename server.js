c4e6e4a2bd4956a3de1a7db68a4ff9ed09592ef8
Ingrid Hammargren <ingrid.hammargren@gmail.com>
2020-06-09 11:15:16 +0200

    added GET/books, sign up and login

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
-
-
-
 import bodyParser from 'body-parser'
 import cors from 'cors'
 import express from 'express'
 import mongoose from 'mongoose'
 import booksData from './books.json'
+import bcrypt from 'bcryptjs'
+import crypto from 'crypto'
 
 const mongoUrl = process.env.MONGO_URL || 'mongodb://localhost/books1'
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
 mongoose.Promise = Promise
+mongoose.set('useCreateIndex', true)
 
 //Create the data models here:
 const User = mongoose.model('User', {
@@ -57,6 +57,23 @@ const seedDatabase = async () => {
 seedDatabase()
 //}
 
+const authenticateUser = async (req, res, next) => {
+  try {
+    const user = await User.findOne({ accessToken: req.header('Authorization') })
+    if (user) {
+      req.user = user
+      next()
+    } else {
+      res.status(401).json({ loggedOut: true, message: "please try to login again" })
+    }
+  } catch (err) {
+    res
+      .status(403)
+      .json({ message: 'access token missing or wrong', errors: err.message })
+  }
+}
+
+
 // Defines the port the app will run on. Defaults to 8080, but can be 
 // overridden when starting the server. For example:
 //
@@ -71,6 +88,49 @@ app.use(bodyParser.json())
 // Start defining your routes here
 app.get('/', (req, res) => {
   res.send('Hello world')
+})
+
+// Get all the books
+app.get('/books', async (req, res) => {
+  const books = await Book.find()
+  res.json(books)
+})
+
+// Create user - sign up
+app.post('/users', async (req, res) => {
+  try {
+    const { name, email, password } = req.body
+    const user = new User({ name, email, password: bcrypt.hashSync(password) })
+    const saved = await user.save()
+    res.status(201).json(saved)
+  } catch (err) {
+    res.status(401).json({ message: 'Could not create user', errors: err.errors })
+  }
+})
+
+// Get one users info/profile page
+app.get('/users/:id', authenticateUser)
+app.get('/users/:id', async (req, res) => {
+  try {
+    res.status(201).json(req.user)
+  } catch (err) {
+    res.status(400).json({ message: 'could not save user', errors: err.message })
+  }
+})
+
+// Log in
+app.post('/sessions', async (req, res) => {
+  try {
+    const { email, password } = req.body
+    const user = await User.findOne({ email })
+    if (user && bcrypt.compareSync(password, user.password)) {
+      res.json({ userId: user._id, accessToken: user.accessToken })
+    } else {
+      res.json({ notFound: true, message: 'wrong username or password' })
+    }
+  } catch (err) {
+    res.status(400).json({ errors: err.errors })
+  }
 })
 
 // Start the server
